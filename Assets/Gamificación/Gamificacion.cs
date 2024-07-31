6e5562c6705bec93be07903f4490593d3d54ef3c
Marcelo <marcelo.guevara.jara@gmail.com>
2024-07-31 09:02:57 -0500

    Implementaciones general de UI

--- Assets/Gamificación/Gamificacion.cs
+++ Assets/Gamificación/Gamificacion.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Gamificacion : MonoBehaviour
 {
     [SerializeField]private GameObject Limite1;
     [SerializeField]private GameObject Limite2;
+    [SerializeField]public float ProductivityPointsTotal = 0;
+    [SerializeField]public float ProductivityPointsDaily = 0;
     public static Gamificacion gamificacionManager;
-   
-
-
+    [SerializeField] private TextMeshProUGUI textoProductivityPointsTotal;
+    [SerializeField] private TextMeshProUGUI textoProductivityPointsDaily;
     public static float MinY { get => Mathf.Min(gamificacionManager.Limite1.transform.position.y, gamificacionManager.Limite2.transform.position.y); }
     public static float MaxY { get => Mathf.Max(gamificacionManager.Limite1.transform.position.y, gamificacionManager.Limite2.transform.position.y); }
     public static float MaxX { get => Mathf.Max(gamificacionManager.Limite1.transform.position.x, gamificacionManager.Limite2.transform.position.x); }
@@ -27,5 +29,9 @@ public class Gamificacion : MonoBehaviour
         
     }
 
+    //private void MostrarProductivityPoints()
+    //{
+    //    textoProductivityPointsDaily.text = ProductivityPointsTotal.ToString();
+    //}
 
 }
