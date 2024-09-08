43f458133342b51a1bb5533f8053f12acdb169bc
Marcelo <marcelo.guevara.jara@gmail.com>
2024-09-08 13:34:52 -0500

    Conseguí usar la API de Gemini
    Mediante el uso del siguiente video y un pequeño cambio de chat gpt al código original, logré que funcionara
    https://www.google.com/search?sca_esv=c4c1450a5839235a&sca_upv=1&sxsrf=ADLYWIJNEHfF90HZHMCyusTySw3U8_-09A:1725820473717&q=how+to+use+gemini+in+C%23+in+unity&tbm=vid&source=lnms&fbs=AEQNm0C5ZD7_oGHQepRYsCtO_UJttEdQFMjv8Pvqott0L0w-yB1ye7Kg1OF98BX6ymUApJHd4wXEZ4-FB8OHpnt-ybguw8HYpiurAHMZ6p6Kh869Tq7-xCnufCVBr1Uz9SkYrCgDyNZlKe5gQx1xB2ox7wYgqEHiLa6PD3MlRZs53wZBV1Cx4n0Y544hc5db30VVgq1SsUdx&sa=X&ved=2ahUKEwimvaCi_rOIAxV_ALkGHftZOUgQ0pQJegQIFRAB&biw=1422&bih=701&dpr=0.9#fpstate=ive&vld=cid:99727885,vid:W_telIOUm5M,st:0

--- Assets/Scenes/Test Gemini AI/TestAI.cs
+++ Assets/Scenes/Test Gemini AI/TestAI.cs
@@ -1,13 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 
 public class TestAI : MonoBehaviour
 {
-    public string Proyect_ID = "pomodoro-a-productivity-game";
-    public string Location = "us-central1";
+    [SerializeField] private string gasURL;
+    [SerializeField] private string prompt;
 
+    private IEnumerator sendDataToGas()
+    {
+        WWWForm form = new WWWForm();
 
+        form.AddField("parameter",prompt);
+
+        UnityWebRequest www = UnityWebRequest.Post(gasURL,form);    
+
+        yield return www.SendWebRequest();
+        string response = "";
+
+        if(www.result == UnityWebRequest.Result.Success)
+        {
+            response = www.downloadHandler.text;
+        }
+        else
+        {
+
+            response = "hubo un error";
+        }
+        Debug.Log(response);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(sendDataToGas());
+        }
+    }
 
 }
